131ae7103873b0b2545efc4848313a2226555715
naruecha.s <naruecha.s@carabao.co.th>
2021-09-19 07:29:29 +0700

    add menu check-order

---
+++ src/pages/check-order.tsx
@@ -0,0 +1,12 @@
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+const CheckOrder = () => {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h1"> Check Order </Typography>
+    </Container>
+  );
+};
+
+export default CheckOrder;
