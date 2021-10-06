d58076270a8aa017561f365005a3c1f72ceb4c52
naruecha.s <naruecha.s@carabao.co.th>
2021-10-06 18:21:16 +0700

    add model Shipment DeliveryOrder and Item

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,40 +1,82 @@
-import { GridRowId } from '@mui/x-data-grid';
 export interface CheckOrderRequest {
-    orderNo: string;
-    orderStatus: string;
-    orderType: string
+  shipmentNo: string;
+  sdNo: string;
+  sdStatus: number;
+  sdType: number;
 }
 
 export interface CheckOrderResponse {
-    ref: number;
-    total: number;
-    orders?: Order[]
+  ref: number;
+  total: number;
+  orders?: Order[];
 }
 
 export interface Order {
-    orderNo: string;
-    orderStatus: string;
-    orderType: string;
-    orderShipment: string;
-    orderTotal: number;
-    orderTote: number;
-    orderCreateDate: string;
-    products?: Product[]
+  orderNo: string;
+  orderStatus: string;
+  orderType: string;
+  orderShipment: string;
+  orderTotal: number;
+  orderTote: number;
+  orderCreateDate: string;
+  products?: Product[];
 }
 
 export interface Product {
-    productId: string;
-    productBarCode: string;
-    productDescription: string;
-    productUnit: string;
-    productQuantityRef: number;
-    productQuantityActual: number;
-    productDifference: number;
+  productId: string;
+  productBarCode: string;
+  productDescription: string;
+  productUnit: string;
+  productQuantityRef: number;
+  productQuantityActual: number;
+  productDifference: number;
+}
+
+export interface Shipment {
+  shipmentNo: string;
+  shipmentDate: string;
+  status: string;
+  sapDocType: string;
+  sdNo: string;
+  sdStatus: number;
+  sdType: number;
+  toteCnt: number;
+  boxCnt: number;
+  entries: DeliveryOrder[];
+}
+
+export interface DeliveryOrder {
+  deliveryOrderNo: string;
+  deliveryOrderDate: string;
+  items: Item[];
+}
+
+export interface Item {
+  productName: string;
+  ItemRefNo: string;
+  barcode: string;
+  skuCode: string;
+  outOfStockStatus: string;
+  toteCode: string;
+  expireDate: string;
+  unitFactor: number;
+  unit: {
+    code: string;
+    name: string;
+  };
+  quantity: {
+    qty: number;
+    qtyAll: number;
+    qtyAllBefore: string;
+    actualQty: number;
+    qtyDiff: number;
+  };
+  comment: string;
 }
 
 export interface OrderProductListProps {
-    // shipment: GridRowId | undefined;
-    shipment: any | undefined;
-    defaultOpen: boolean;
-    onClickClose: any;
+  // shipment: GridRowId | undefined;
+  shipment: any | undefined;
+  defaultOpen: boolean;
+  onClickClose: any;
 }
