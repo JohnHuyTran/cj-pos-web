0e14e75c96dc56bc6dd191b400a352ef80dbcf0a
Naruecha Samritwong <naruecha.s@carabao.co.th>
2021-10-14 16:58:25 +0700

    add search criteria and call backend from adapter

--- src/store/slices/check-order-slice.ts
+++ src/store/slices/check-order-slice.ts
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import { getOrderList } from '../../mockdata/orders';
 import { ShipmentRequest, ShipmentResponse } from '../../models/order-model';
+import { environment } from '../../environment-base';
+import { get } from '../../adapters/posback-adapter';
 
 type State = {
   orderList: ShipmentResponse;
@@ -27,7 +29,28 @@ export const featchOrderListAsync = createAsyncThunk(
   'orderList',
   async (payload: ShipmentRequest) => {
     try {
-      const response: ShipmentResponse = await getOrderList(payload).then();
+      const apiRootPath = environment.orders.shipment.fetchOrder.url;
+      let path = `${apiRootPath}?limit=10&page=1`;
+      if (payload.shipmentNo) {
+        path = path + `&shipmentNo=${payload.shipmentNo}`;
+      }
+      if (payload.sdNo) {
+        path = path + `&sdNo=${payload.sdNo}`;
+      }
+      if (payload.sdStatus == 0 || payload.sdStatus == 1) {
+        path = path + `&sdStatus=${payload.sdStatus}`;
+      }
+      if (payload.sdType == 0 || payload.sdType == 1) {
+        path = path + `&sdType=${payload.sdType}`;
+      }
+      if (payload.dateFrom) {
+        path = path + `&dateFrom=${payload.dateFrom}`;
+      }
+      if (payload.dateTo) {
+        path = path + `&dateTo=${payload.dateTo}`;
+      }
+      console.log('path : ', path);
+      const response: ShipmentResponse = await get(path).then();
       return response;
     } catch (error) {
       throw error;
@@ -40,7 +63,7 @@ const checkOrderSlice = createSlice({
   initialState,
   reducers: {
     clearDataFilter: (state) => {
-      state.orderList;
+      state = initialState;
     },
   },
   extraReducers: (builer) => {
