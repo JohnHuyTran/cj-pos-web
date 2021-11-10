d329b4c6170ff88ee44a68c9dfd699475c637fdf
Supattra Buathong <supattra.bu@tdshop.io>
2021-11-10 08:41:49 +0700

    serch DC check order

--- src/components/dc-check-orders/dc-check-order.tsx
+++ src/components/dc-check-orders/dc-check-order.tsx
@@ -10,67 +10,123 @@ import Select from "@mui/material/Select";
 import { Button } from "@mui/material";
 import { useAppSelector, useAppDispatch } from "../../store/store";
 import {
-  featchOrderListAsync,
+  featchOrderListDcAsync,
   clearDataFilter,
-} from "../../store/slices/check-order-slice";
-import {
-  saveSearchCriteria,
-  clearSearchCriteria,
-} from "../../store/slices/save-search-order";
-import { ShipmentRequest } from "../../models/order-model";
+} from "../../store/slices/dc-check-order-slice";
+import { featchBranchListAsync } from "../../store/slices/search-branches-slice";
+import { clearSearchCriteriaDc } from "../../store/slices/save-search-order-dc-slice";
+import { saveSearchCriteriaDc } from "../../store/slices/save-search-order-dc-slice";
+import { CheckOrderRequest } from "../../models/dc-check-order-model";
 import DCOrderList from "./dc-order-list";
+import { useStyles } from "../../styles/makeTheme";
 import DatePickerComponent from "../commons/ui/date-picker";
+import LoadingModal from "../commons/ui/loading-modal";
+import { SearchOff } from "@mui/icons-material";
 
-moment.locale("en");
+moment.locale("th");
 
 interface State {
-  orderShipment: string;
-  orderNo: string;
-  orderStatus: string;
-  orderType: string;
+  shipmentNo: string;
+  branchCode: string;
+  verifyDCStatus: string;
   dateFrom: string;
   dateTo: string;
+  sdType: string;
+  sortBy: string;
+}
+interface loadingModalState {
+  open: boolean;
 }
 
 function DCCheckOrderSearch() {
+  const classes = useStyles();
   const dispatch = useAppDispatch();
-  const items = useAppSelector((state) => state.checkOrderList);
+  const items = useAppSelector((state) => state.dcCheckOrderList);
   const [values, setValues] = React.useState<State>({
-    orderShipment: "",
-    orderNo: "",
-    orderStatus: "ALL",
-    orderType: "ALL",
-    dateFrom: "10/10/2021",
-    dateTo: "11/10/2021",
+    shipmentNo: "",
+    branchCode: "ALL",
+    verifyDCStatus: "ALL",
+    dateFrom: "",
+    dateTo: "",
+    sdType: "ALL",
+    sortBy: "",
   });
   const [startDate, setStartDate] = React.useState<Date | null>(new Date());
   const [endDate, setEndDate] = React.useState<Date | null>(new Date());
+  const [openLoadingModal, setOpenLoadingModal] =
+    React.useState<loadingModalState>({
+      open: false,
+    });
+
+  useEffect(() => {
+    dispatch(featchBranchListAsync());
+  }, []);
+
+  const branchList = useAppSelector((state) => state.searchBranchSlice);
+
+  // console.log("branchList: ", branchList.branchList.data);
 
   const handleChange = (event: any) => {
     const value = event.target.value;
     setValues({ ...values, [event.target.name]: value });
-    console.log(values);
+    // console.log("value in handle change: ", values);
   };
 
-  const onClickSearchBtn = () => {
-    const payload: ShipmentRequest = {
+  const handleOpenLoading = (prop: any, event: boolean) => {
+    setOpenLoadingModal({ ...openLoadingModal, [prop]: event });
+  };
+
+  const onClickSearchBtn = async () => {
+    const payload: CheckOrderRequest = {
       limit: "10",
       page: "1",
-      shipmentNo: values.orderShipment,
-      sdNo: values.orderNo,
-      dateFrom: moment(startDate).format("DD/MM/YYYY"),
-      dateTo: moment(endDate).format("DD/MM/YYYY"),
-      sdStatus: parseInt(values.orderStatus),
-      sdType: parseInt(values.orderType),
+      shipmentNo: values.shipmentNo,
+      branchCode: values.branchCode,
+      verifyDCStatus: values.verifyDCStatus,
+      dateFrom: moment(startDate).startOf("day").toISOString(),
+      dateTo: moment(endDate).endOf("day").toISOString(),
+      sdType: values.sdType,
+      sortBy: values.sortBy,
+      clearSearch: false,
     };
-    dispatch(featchOrderListAsync(payload));
-    dispatch(saveSearchCriteria(payload));
-    console.log(`Search Criteria: ${JSON.stringify(payload)}`);
+
+    handleOpenLoading("open", true);
+    await dispatch(featchOrderListDcAsync(payload));
+    await dispatch(saveSearchCriteriaDc(payload));
+    handleOpenLoading("open", false);
+
+    // console.log(`Search Criteria: ${JSON.stringify(payload)}`);
   };
 
-  const onClickClearBtn = () => {
-    dispatch(clearDataFilter());
-    dispatch(clearSearchCriteria());
+  const onClickClearBtn = async () => {
+    setStartDate(new Date());
+    setEndDate(new Date());
+    setValues({
+      shipmentNo: "",
+      branchCode: "ALL",
+      verifyDCStatus: "ALL",
+      dateFrom: "",
+      dateTo: "",
+      sdType: "ALL",
+      sortBy: "",
+    });
+
+    // items.orderList = '';
+
+    const payload: CheckOrderRequest = {
+      limit: "10",
+      page: "1",
+      shipmentNo: values.shipmentNo,
+      branchCode: values.branchCode,
+      verifyDCStatus: values.verifyDCStatus,
+      dateFrom: moment(startDate).startOf("day").toISOString(),
+      dateTo: moment(endDate).endOf("day").toISOString(),
+      sdType: values.sdType,
+      sortBy: values.sortBy,
+      clearSearch: true,
+    };
+
+    dispatch(featchOrderListDcAsync(payload));
   };
 
   // useEffect(() => {
@@ -85,35 +141,68 @@ function DCCheckOrderSearch() {
     setEndDate(value);
   };
 
+  let orderListData;
+  const orderListDatas = items.orderList.data;
+
+  if (orderListDatas.length === 0) {
+    orderListData = (
+      <Grid item container xs={12} justifyContent="center">
+        <Box color="#CBD4DB">
+          <h2>
+            ไม่มีข้อมูล <SearchOff fontSize="large" />
+          </h2>
+        </Box>
+      </Grid>
+    );
+  } else {
+    orderListData = <DCOrderList />;
+  }
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              ค้นหา
+        <Grid container rowSpacing={3} columnSpacing={{ xs: 7 }}>
+          <Grid item xs={4}>
+            <Typography gutterBottom variant="subtitle1" component="div" mb={1}>
+              ค้นหาเอกสาร
             </Typography>
             <TextField
-              sx={{ width: "480px" }}
-              placeholder="เลขที่เอกสาร LD หรือ เลขที่เอกสาร SD"
-              id="txtDocumentNO"
+              id="txtShipmentNo"
+              name="shipmentNo"
               size="small"
-              name="documentNo"
+              value={values.shipmentNo}
               onChange={handleChange}
+              className={classes.MtextField}
+              fullWidth
+              placeholder="เลขที่เอกสาร LD/เลขที่เอกสาร SD"
             />
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              สาขา
+          <Grid item xs={4}>
+            <Typography gutterBottom variant="subtitle1" component="div" mb={1}>
+              สาขาปลายทาง
             </Typography>
-            <FormControl sx={{ width: 193 }}>
+            <FormControl fullWidth className={classes.Mselect}>
+              <Select
+                id="selBranchNo"
+                name="branchNo"
+                value={values.branchCode}
+                inputProps={{ "aria-label": "Without label" }}
+              >
+                <MenuItem value={"ALL"} selected={true}>
+                  ทั้งหมด
+                </MenuItem>
+                {branchList.branchList.data.map((option) => (
+                  <MenuItem value={option.code}>{option.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            {/* <FormControl fullWidth className={classes.Mselect}>
               <Select
                 id="selBranchNo"
                 name="branchNo"
                 value={values.orderStatus}
-                onChange={handleChange}
                 inputProps={{ "aria-label": "Without label" }}
-                autoWidth={true}
               >
                 <MenuItem value={"ALL"} selected={true}>
                   ทั้งหมด
@@ -121,53 +210,75 @@ function DCCheckOrderSearch() {
                 <MenuItem value={"0"}>วัดโคก</MenuItem>
                 <MenuItem value={"1"}>ซอยใจเอื้อ</MenuItem>
               </Select>
-            </FormControl>
+            </FormControl> */}
+            {/* <TextField
+              id="txtBranchCode"
+              name="branchCode"
+              size="small"
+              value={values.branchCode}
+              onChange={handleChange}
+              className={classes.MtextField}
+              fullWidth
+              placeholder="branchCode"
+            /> */}
           </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              วันที่รับสินค้า ตั้งแต่
+          <Grid item xs={4}>
+            <Typography gutterBottom variant="subtitle1" component="div" mb={1}>
+              สถานะการตรวจสอบผลต่าง
             </Typography>
-            {/* <DatePickerComponent onClickDate={handleStartDatePicker} value={startDate} /> */}
-          </Grid>
-          <Grid item xs={9}>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              ถึง
-            </Typography>
-            {/* <DatePickerComponent onClickDate={handleEndDatePicker} value={endDate} /> */}
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              สถานะ
-            </Typography>
-            <FormControl sx={{ width: 193 }}>
+            <FormControl fullWidth className={classes.Mselect}>
               <Select
-                id="selOrderStatus"
-                name="orderStatus"
-                value={values.orderStatus}
+                id="selVerifyDCStatus"
+                name="verifyDCStatus"
+                value={values.verifyDCStatus}
                 onChange={handleChange}
                 inputProps={{ "aria-label": "Without label" }}
-                autoWidth={true}
               >
                 <MenuItem value={"ALL"} selected={true}>
                   ทั้งหมด
                 </MenuItem>
-                <MenuItem value={"0"}>บันทึก</MenuItem>
-                <MenuItem value={"1"}>อนุมัติ</MenuItem>
+                <MenuItem value={"0"}>รอการตรวจสอบ</MenuItem>
+                <MenuItem value={"1"}>ตรวจสอบแล้ว</MenuItem>
               </Select>
             </FormControl>
           </Grid>
-          <Grid item xs={9}>
-            <Typography variant="subtitle1" gutterBottom component="div">
+
+          <Grid item xs={4} sx={{ pt: 30 }}>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              วันที่รับสินค้า
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              ตั้งแต่
+            </Typography>
+            <DatePickerComponent
+              onClickDate={handleStartDatePicker}
+              value={startDate}
+            />
+          </Grid>
+          <Grid item xs={4} container alignItems="flex-end">
+            <Box sx={{ width: "100%" }}>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                ถึง
+              </Typography>
+              <DatePickerComponent
+                onClickDate={handleEndDatePicker}
+                value={endDate}
+                type={"TO"}
+                minDateTo={startDate}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={4} container alignItems="flex-end">
+            <Typography gutterBottom variant="subtitle1" component="div">
               ประเภท
             </Typography>
-            <FormControl sx={{ width: 193 }}>
+            <FormControl fullWidth className={classes.Mselect}>
               <Select
-                id="selOrderType"
-                name="orderType"
-                value={values.orderType}
+                id="selSdType"
+                name="sdType"
+                value={values.sdType}
                 onChange={handleChange}
                 inputProps={{ "aria-label": "Without label" }}
-                autoWidth={true}
               >
                 <MenuItem value={"ALL"} selected={true}>
                   ทั้งหมด
@@ -177,29 +288,44 @@ function DCCheckOrderSearch() {
               </Select>
             </FormControl>
           </Grid>
-          <Grid item xs={12}>
-            <Box
-              sx={{
-                width: "150px",
-                display: "flex",
-                justifyContent: "space-between",
-              }}
+
+          <Grid
+            item
+            container
+            xs={12}
+            justifyContent="flex-end"
+            direction="row"
+            alignItems="flex-end"
+          >
+            <Button
+              id="btnClear"
+              variant="contained"
+              size="large"
+              onClick={onClickClearBtn}
+              sx={{ width: "15%" }}
+              className={classes.MbtnClear}
             >
-              <Button id="btnSearch" variant="contained" color="primary">
-                ค้นหา
-              </Button>
-              <Button
-                id="btnClear"
-                variant="contained"
-                sx={{ backgroundColor: "#AEAEAE" }}
-              >
-                เคลียร์
-              </Button>
-            </Box>
+              เคลียร์
+            </Button>
+            <Button
+              id="btnSearch"
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={onClickSearchBtn}
+              sx={{ width: "15%", ml: 2 }}
+              className={classes.MbtnSearch}
+            >
+              ค้นหา
+            </Button>
           </Grid>
         </Grid>
       </Box>
-      {items.orderList && <DCOrderList />}
+
+      <Box mt={6}></Box>
+      {/* {items.orderList && <DCOrderList />} */}
+      {orderListData}
+      <LoadingModal open={openLoadingModal.open} />
     </>
   );
 }
