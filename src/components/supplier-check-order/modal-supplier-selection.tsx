fc57b65e35d31f5475176c7c45c46634dc8c9dfc
Phumai <mcc3939@gmail.com>
2021-12-07 10:51:55 +0700

    fix

--- src/components/supplier-check-order/modal-supplier-selection.tsx
+++ src/components/supplier-check-order/modal-supplier-selection.tsx
@@ -1,57 +1,61 @@
-import React from "react";
-import { styled } from "@mui/material/styles";
-import { makeStyles } from "@material-ui/core";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
-import IconButton from "@mui/material/IconButton";
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import Radio from "@mui/material/Radio";
-import RadioGroup, { useRadioGroup } from "@mui/material/RadioGroup";
-import FormControlLabel, {
-  FormControlLabelProps,
-} from "@mui/material/FormControlLabel";
-import Button from "@mui/material/Button";
-import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Divider from "@mui/material/Divider";
-import ListItemText from "@mui/material/ListItemText";
-import Typography from "@mui/material/Typography";
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import { makeStyles } from '@material-ui/core';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Radio from '@mui/material/Radio';
+import RadioGroup, { useRadioGroup } from '@mui/material/RadioGroup';
+import FormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
 
-import { useAppDispatch } from "../../store/store";
-import { changeState } from "../../store/slices/supplier-selection-slice";
-import { typography } from "@mui/system";
+import { useAppDispatch } from '../../store/store';
+import { changeState } from '../../store/slices/supplier-selection-slice';
+
+interface Supplier {
+  label: string;
+  code: string;
+  po?: string[];
+}
 
 const mockDataset = [
   {
-    label: "บริษัท เบทาโกรการเกษตรอุตสาหกรรม จำกัด",
-    code: "401212254",
-    po: ["401212254", "401224456", "P121100101-000163"],
+    label: 'บริษัท เบทาโกรการเกษตรอุตสาหกรรม จำกัด',
+    code: '401212254',
+    po: ['401212254', '401224456', 'P121100101-000163'],
   },
   {
-    label: "บริษัท เบทาไมค์อิเล็กทริคจำกัด",
-    code: "401224456",
+    label: 'บริษัท เบทาไมค์อิเล็กทริคจำกัด',
+    code: '401224456',
   },
 ];
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
+  '& .MuiDialogContent-root': {
     padding: theme.spacing(2),
   },
 }));
 
 const useStyles = makeStyles((theme) => ({
   MTextField: {
-    "& .MuiOutlinedInput-root": {
-      borderRadius: "5px !important",
+    '& .MuiOutlinedInput-root': {
+      borderRadius: '5px !important',
       padding: theme.spacing(1.8),
     },
   },
   MBtnAddSupplier: {
-    borderRadius: "5px !important",
+    borderRadius: '5px !important',
   },
   textLabelInput: { fontSize: 14, fontWeight: 400 },
   textListSupplier: {
@@ -67,13 +71,13 @@ interface StyledFormControlLabelProps extends FormControlLabelProps {
   checked: boolean;
 }
 
-const StyledFormControlLabel = styled((props: StyledFormControlLabelProps) => (
-  <FormControlLabel {...props} />
-))(({ theme, checked }) => ({
-  ".MuiFormControlLabel-label": checked && {
-    color: theme.palette.primary.main,
-  },
-}));
+const StyledFormControlLabel = styled((props: StyledFormControlLabelProps) => <FormControlLabel {...props} />)(
+  ({ theme, checked }) => ({
+    '.MuiFormControlLabel-label': checked && {
+      color: theme.palette.primary.main,
+    },
+  })
+);
 
 function MyFormControlLabel(props: FormControlLabelProps) {
   const radioGroup = useRadioGroup();
@@ -107,35 +111,64 @@ const BootstrapDialogTitle = (props: DialogTitleProps) => {
           aria-label="close"
           onClick={onClose}
           sx={{
-            position: "absolute",
+            position: 'absolute',
             right: 8,
             top: 8,
             color: (theme) => theme.palette.cancelColor.main,
           }}
         >
-          <CancelOutlinedIcon
-            fontSize="large"
-            stroke={"white"}
-            stroke-width={1}
-          />
+          <CancelOutlinedIcon fontSize="large" stroke={'white'} stroke-width={1} />
         </IconButton>
       ) : null}
     </DialogTitle>
   );
 };
 
-export default function ModalSupplierSelection({
-  openModal,
-  handleCloseModal,
-}: Props) {
+function sleep(delay = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+export default function ModalSupplierSelection({ openModal, handleCloseModal }: Props) {
   const classes = useStyles();
   const dispatch = useAppDispatch();
   const [hasPO, setHasPO] = React.useState<boolean>(false);
-  const [hasPOValue, setHasPOValue] = React.useState<string>("");
+  const [hasPOValue, setHasPOValue] = React.useState<string>('');
+
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState<Supplier[]>([]);
+  const loading = open && options.length === 0;
+
+  React.useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      await sleep(1e3); // For demo purposes.
+
+      if (active) {
+        setOptions([...mockDataset]);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
 
   const autocompleteRenderListItem = (props: any, option: any) => {
     return (
-      <List {...props} sx={{ width: "100%" }} key={option.code}>
+      <List {...props} sx={{ width: '100%' }} key={option.code}>
         <ListItem alignItems="flex-start" disablePadding>
           <ListItemText primary={option.label} secondary={option.code} />
         </ListItem>
@@ -143,36 +176,36 @@ export default function ModalSupplierSelection({
     );
   };
 
-  const onChangeSelection = (_: any, value: any) => {
-    if (value) {
-      if (value.po && value.po.length > 0) {
+  const onChangeSelection = (_: any, option: any) => {
+    if (option) {
+      if (option.po && option.po.length > 0) {
         setHasPO(true);
-        setHasPOValue("มีเอกสาร PO");
+        setHasPOValue('มีเอกสาร PO');
       } else {
         setHasPO(false);
-        setHasPOValue("ไม่มีมีเอกสาร PO");
+        setHasPOValue('ไม่มีมีเอกสาร PO');
       }
     } else {
       setHasPO(false);
-      setHasPOValue("");
+      setHasPOValue('');
     }
   };
 
   const onSubmitData = () => {
     setHasPO(false);
-    setHasPOValue("");
+    setHasPOValue('');
     handleCloseModal();
     dispatch(changeState(mockDataset[0]));
   };
 
   const onCloseModal = () => {
     setHasPO(false);
-    setHasPOValue("");
+    setHasPOValue('');
     handleCloseModal();
   };
 
   const filterOptions = createFilterOptions({
-    stringify: (option: any) => option.label + option.code,
+    stringify: (option: Supplier) => option.label + option.code,
   });
 
   return (
@@ -185,26 +218,33 @@ export default function ModalSupplierSelection({
         fullWidth
         maxWidth="sm"
       >
-        <BootstrapDialogTitle
-          id="customized-dialog-title"
-          onClose={onCloseModal}
-        >
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={onCloseModal}>
           เพิ่มผู้จำหน่าย
         </BootstrapDialogTitle>
 
         <DialogContent>
-          <Box sx={{ display: "flex" }}>
+          <Box sx={{ display: 'flex' }}>
             <Box sx={{ flex: 3 }}>
               <label className={classes.textLabelInput}>ผู้จำหน่าย</label>
               <Autocomplete
                 id="searchSupplierModal"
                 fullWidth
                 freeSolo
+                loading={true}
+                loadingText="กำลังโหลด..."
                 sx={{ mt: 1 }}
-                options={mockDataset}
+                open={open}
+                onOpen={() => {
+                  setOpen(true);
+                }}
+                onClose={() => {
+                  setOpen(false);
+                }}
+                options={options}
                 filterOptions={filterOptions}
                 renderOption={autocompleteRenderListItem}
                 onChange={onChangeSelection}
+                isOptionEqualToValue={(option, value) => option.label === value.label}
                 renderInput={(params) => (
                   <TextField
                     {...params}
@@ -212,6 +252,15 @@ export default function ModalSupplierSelection({
                     className={classes.MTextField}
                     variant="outlined"
                     fullWidth
+                    InputProps={{
+                      ...params.InputProps,
+                      endAdornment: (
+                        <>
+                          {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                          {params.InputProps.endAdornment}
+                        </>
+                      ),
+                    }}
                   />
                 )}
               />
@@ -219,42 +268,23 @@ export default function ModalSupplierSelection({
 
             <Box sx={{ flex: 2, ml: 2 }}>
               <label className={classes.textLabelInput}>ประเภทผู้จำหน่าย</label>
-              <TextField
-                id="supplierModalType"
-                sx={{ mt: 1 }}
-                className={classes.MTextField}
-                value={hasPOValue}
-              />
+              <TextField id="supplierModalType" sx={{ mt: 1 }} className={classes.MTextField} value={hasPOValue} />
             </Box>
           </Box>
 
-          <Box sx={{ mt: 4, visibility: hasPO ? "visible" : "hidden" }}>
-            <label className={classes.textListSupplier}>
-              รายการเอกสารใบสั่งซื้อ PO
-            </label>
+          <Box sx={{ mt: 4, visibility: hasPO ? 'visible' : 'hidden' }}>
+            <label className={classes.textListSupplier}>รายการเอกสารใบสั่งซื้อ PO</label>
 
-            <RadioGroup
-              name="use-radio-group"
-              defaultValue="first"
-              id="listSupplierDocPO"
-            >
-              <MyFormControlLabel
-                value="401212254"
-                label="401212254"
-                control={<Radio />}
-              />
-              <MyFormControlLabel
-                value="401224456"
-                label="401224456"
-                control={<Radio />}
-              />
+            <RadioGroup name="use-radio-group" defaultValue="first" id="listSupplierDocPO">
+              <MyFormControlLabel value="401212254" label="401212254" control={<Radio />} />
+              <MyFormControlLabel value="401224456" label="401224456" control={<Radio />} />
             </RadioGroup>
           </Box>
 
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "row-reverse",
+              display: 'flex',
+              flexDirection: 'row-reverse',
               p: 1,
             }}
           >
