03db64401b1001101583f03b9c57c7b91514d6b6
thiranan.jit <thiranan.jit@cjexpress.co.th>
2022-02-23 10:45:55 +0700

    clear state view

--- src/components/stock-transfer/stock-transfer-rt.tsx
+++ src/components/stock-transfer/stock-transfer-rt.tsx
@@ -17,6 +17,7 @@ import { saveSearchStockTransferRt } from '../../store/slices/save-search-stock-
 import StockTransferRtList from './stock-transfer-rt-list';
 import ModalCreateStockTransfer from './stock-request-detail';
 import { updateAddItemsState } from '../../store/slices/add-items-slice';
+import { updatestockRequestItemsState } from '../../store/slices/stock-request-items-slice';
 import { isAllowActionPermission, isAllowMainMenuPermission, isGroupBranch } from '../../utils/role-permission';
 import { env } from '../../adapters/environmentConfigs';
 import { getBranchName } from '../../utils/utils';
@@ -224,8 +225,8 @@ export default function StockTransferRt() {
       orderListData = <StockTransferRtList />;
     } else {
       orderListData = (
-        <Grid item container xs={12} justifyContent='center'>
-          <Box color='#CBD4DB'>
+        <Grid item container xs={12} justifyContent="center">
+          <Box color="#CBD4DB">
             <h2>
               {/* ไม่มีข้อมูล <SearchOff fontSize='large' /> */}
               ไม่มีข้อมูล
@@ -240,6 +241,7 @@ export default function StockTransferRt() {
   const [typeModal, setTypeModal] = React.useState('Create');
   const handleOpenCreateModal = async () => {
     await dispatch(updateAddItemsState({}));
+    await dispatch(updatestockRequestItemsState({}));
     setTypeModal('Create');
     setOpenCreateModal(true);
   };
@@ -253,22 +255,22 @@ export default function StockTransferRt() {
       <Box>
         <Grid container rowSpacing={3} columnSpacing={{ xs: 7 }}>
           <Grid item xs={4}>
-            <Typography gutterBottom variant='subtitle1' component='div' mb={1}>
+            <Typography gutterBottom variant="subtitle1" component="div" mb={1}>
               {t('documentSearch')}
             </Typography>
             <TextField
-              id='txtDocNo'
-              name='docNo'
-              size='small'
+              id="txtDocNo"
+              name="docNo"
+              size="small"
               value={values.docNo}
               onChange={handleChange}
               className={classes.MtextField}
               fullWidth
-              placeholder='เลขที่เอกสาร RT'
+              placeholder="เลขที่เอกสาร RT"
             />
           </Grid>
           <Grid item xs={4}>
-            <Typography gutterBottom variant='subtitle1' component='div' mb={1}>
+            <Typography gutterBottom variant="subtitle1" component="div" mb={1}>
               สาขาต้นทาง
             </Typography>
             <BranchListDropDown
@@ -280,7 +282,7 @@ export default function StockTransferRt() {
             />
           </Grid>
           <Grid item xs={4}>
-            <Typography gutterBottom variant='subtitle1' component='div' mb={1}>
+            <Typography gutterBottom variant="subtitle1" component="div" mb={1}>
               สาขาปลายทาง
             </Typography>
             <BranchListDropDown
@@ -292,16 +294,16 @@ export default function StockTransferRt() {
           </Grid>
 
           <Grid item xs={4} sx={{ pt: 30 }}>
-            <Typography gutterBottom variant='subtitle1' component='div'>
+            <Typography gutterBottom variant="subtitle1" component="div">
               วันที่โอน
             </Typography>
-            <Typography gutterBottom variant='subtitle1' component='div'>
+            <Typography gutterBottom variant="subtitle1" component="div">
               ตั้งแต่*
             </Typography>
             <DatePickerAllComponent onClickDate={handleStartDatePicker} value={startDate} />
           </Grid>
           <Grid item xs={4}>
-            <Typography gutterBottom variant='subtitle1' component='div' sx={{ mt: 3.5 }}>
+            <Typography gutterBottom variant="subtitle1" component="div" sx={{ mt: 3.5 }}>
               ถึง*
             </Typography>
             <DatePickerAllComponent
@@ -312,16 +314,17 @@ export default function StockTransferRt() {
             />
           </Grid>
           <Grid item xs={4} container>
-            <Typography gutterBottom variant='subtitle1' component='div' sx={{ mt: 3.5 }}>
+            <Typography gutterBottom variant="subtitle1" component="div" sx={{ mt: 3.5 }}>
               สถานะ
             </Typography>
             <FormControl fullWidth className={classes.Mselect}>
               <Select
-                id='selPiType'
-                name='statuses'
+                id="selPiType"
+                name="statuses"
                 value={values.statuses}
                 onChange={handleChange}
-                inputProps={{ 'aria-label': 'Without label' }}>
+                inputProps={{ 'aria-label': 'Without label' }}
+              >
                 <MenuItem value={'ALL'} selected={true}>
                   ทั้งหมด
                 </MenuItem>
@@ -333,39 +336,42 @@ export default function StockTransferRt() {
           </Grid>
 
           <Grid item xs={4} sx={{ pt: 30 }}>
-            <Typography gutterBottom variant='subtitle1' component='div' mb={1}>
+            <Typography gutterBottom variant="subtitle1" component="div" mb={1}>
               สาเหตุการโอน
             </Typography>
             <ReasonsListDropDown onChangeReasons={handleChangeReasons} isClear={clearBranchDropDown} />
           </Grid>
 
-          <Grid item container xs={12} sx={{ mt: 3 }} justifyContent='flex-end' direction='row' alignItems='flex-end'>
+          <Grid item container xs={12} sx={{ mt: 3 }} justifyContent="flex-end" direction="row" alignItems="flex-end">
             <Button
-              id='btnCreateStockTransferModal'
-              variant='contained'
+              id="btnCreateStockTransferModal"
+              variant="contained"
               onClick={handleOpenCreateModal}
               sx={{ minWidth: '15%', display: `${displayBtnCreate ? 'none' : ''}` }}
               className={classes.MbtnClear}
               startIcon={<AddCircleOutlineOutlinedIcon />}
-              color='secondary'>
+              color="secondary"
+            >
               สร้างรายการโอน
             </Button>
             <Button
-              id='btnClear'
-              variant='contained'
+              id="btnClear"
+              variant="contained"
               onClick={onClickClearBtn}
               sx={{ width: '13%', ml: 2 }}
               className={classes.MbtnClear}
-              color='cancelColor'>
+              color="cancelColor"
+            >
               เคลียร์
             </Button>
             <Button
-              id='btnSearch'
-              variant='contained'
-              color='primary'
+              id="btnSearch"
+              variant="contained"
+              color="primary"
               onClick={onClickValidateForm}
               sx={{ width: '13%', ml: 2 }}
-              className={classes.MbtnSearch}>
+              className={classes.MbtnSearch}
+            >
               ค้นหา
             </Button>
           </Grid>
