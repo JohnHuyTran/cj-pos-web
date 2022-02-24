a13abbe8a2f5ea7f89097bf474978b66fb63dce4
Nungruthai Pangnuja <nungruthai.pang@cjexpress.co.th>
2022-02-24 18:12:03 +0700

    add group area-manager

--- src/utils/role-permission.ts
+++ src/utils/role-permission.ts
@@ -2,11 +2,10 @@ import { KeyCloakTokenInfo } from '../models/keycolak-token-info';
 import { getUserInfo } from '../store/sessionStore';
 import {
   ACTIONS,
+  KEYCLOAK_GROUP_AREA_MANAGER01,
   KEYCLOAK_GROUP_BRANCH_MANAGER,
   KEYCLOAK_GROUP_BRANCH_MANAGER01,
   KEYCLOAK_GROUP_DC01,
-  KEYCLOAK_GROUP_OC1,
-  KEYCLOAK_GROUP_SCM,
   MAINMENU,
   PERMISSION_GROUP,
   SUBMENU,
@@ -23,10 +22,12 @@ export const getUserGroup = (groups: string[]) => {
     return PERMISSION_GROUP.DC;
   } else if (group === KEYCLOAK_GROUP_BRANCH_MANAGER01 || group === KEYCLOAK_GROUP_BRANCH_MANAGER) {
     return PERMISSION_GROUP.BRANCH;
-  } else if (group === KEYCLOAK_GROUP_SCM) {
+  } else if (group === KEYCLOAK_GROUP_DC01) {
     return PERMISSION_GROUP.SCM;
-  } else if (group === KEYCLOAK_GROUP_OC1) {
+  } else if (group === KEYCLOAK_GROUP_DC01) {
     return PERMISSION_GROUP.OC;
+  } else if (group === KEYCLOAK_GROUP_AREA_MANAGER01) {
+    return PERMISSION_GROUP.AREA_MANAGER;
   }
 
   return '';
@@ -106,6 +107,30 @@ const permission = {
     action: [ACTIONS.STOCK_BT_VIEW, ACTIONS.STOCK_BT_EXPORT],
   },
   branch: {
+    menu: {
+      mainmenu: [MAINMENU.SALE, MAINMENU.STOCK_TRANSFER, MAINMENU.ORDER_RECEIVE],
+      submenu: [
+        SUBMENU.SALE_DISCOUNT,
+        SUBMENU.OR_ORDER_RECEIVE,
+        SUBMENU.OR_DIFF,
+        SUBMENU.OR_SUPPLIER,
+        SUBMENU.ST_REQUEST,
+        SUBMENU.ST_TRANSFER,
+      ],
+    },
+    action: [
+      ACTIONS.STOCK_RT_VIEW,
+      ACTIONS.STOCK_RT_MANAGE,
+      ACTIONS.STOCK_RT_SEND,
+      ACTIONS.STOCK_RT_APPROVE,
+      ACTIONS.STOCK_RT_REJECT,
+      ACTIONS.STOCK_BT_VIEW,
+      ACTIONS.STOCK_BT_MANAGE,
+      ACTIONS.STOCK_BT_SAVEDC,
+      ACTIONS.STOCK_BT_EXPORT,
+    ],
+  },
+  areaManager: {
     menu: {
       mainmenu: [MAINMENU.SALE, MAINMENU.STOCK_TRANSFER, MAINMENU.ORDER_RECEIVE],
       submenu: [
