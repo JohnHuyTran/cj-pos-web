c9be3ab11218b7a36137c894a813a2c1841cf99c
Supattra Buathong <a.supattra.nam@gmail.com>
2022-03-08 22:12:50 +0700

    no message

--- src/components/check-orders/check-order-confirm-model.tsx
+++ src/components/check-orders/check-order-confirm-model.tsx
@@ -224,7 +224,7 @@ export default function CheckOrderConfirmModel(props: ConfirmOrderShipment) {
                   ยืนยันอนุมัติใบตรวจสอบการรับ-โอนสินค้า
                 </Typography>
                 <Typography variant="body1" align="center">
-                  เลขที่เอกสาร LD <label style={{ color: '#AEAEAE' }}>|</label>{' '}
+                  เลขที่เอกสาร <label style={{ color: '#AEAEAE' }}>|</label>{' '}
                   <label style={{ color: '#36C690' }}>
                     <b>{shipmentNo}</b>
                   </label>
