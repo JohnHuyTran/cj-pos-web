1b03aa462a1ce4f0c1ba9b29ade78c52658dc1e2
Nungruthai Pangnuja <nungruthai.pang@cjexpress.co.th>
2022-03-17 16:30:05 +0700

    handle disable btn

--- src/components/tax-invoice/tax-invoice-search.tsx
+++ src/components/tax-invoice/tax-invoice-search.tsx
@@ -63,6 +63,7 @@ export default function TaxInvoiceSearch() {
       docNo: values.docNo,
     };
     await dispatch(savePayloadSearchList(payload));
+    setDisableSearchBtn(true);
     setFlagSearch(false);
   };
 
