0fa350afff2059f1952da1c725d151343af178cb
ndquang7 <ndquang7@cmcglobal.vn>
2022-03-24 10:57:59 +0700

    UI for transfer out search

--- src/pages/transfer-out/transfer-out.tsx
+++ src/pages/transfer-out/transfer-out.tsx
@@ -1,12 +1,12 @@
 import Container from '@mui/material/Container';
 import TitleHeader from '../../components/title-header';
-// import TransferOutSearch from "./transfer-out-search";
+import TransferOutSearch from './transfer-out-search';
 
 const TransferOut = () => {
   return (
     <Container maxWidth="xl">
       <TitleHeader title={'ใช้ในการทำกิจกรรม'} />
-      {/* <TransferOutSearch /> */}
+      <TransferOutSearch />
     </Container>
   );
 };
