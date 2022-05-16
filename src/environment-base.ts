61ac253e0e426b87f0157b1facabc05a662d0e7e
Nungruthai Pangnuja <nungruthai.pang@cjexpress.co.th>
2022-05-16 15:30:14 +0700

    test wrong url in notification

--- src/environment-base.ts
+++ src/environment-base.ts
@@ -73,7 +73,7 @@ export const environment = {
         url: '/task/notifications/reminders',
       },
       announcements: {
-        url: '/tasktest/notifications/announcements',
+        url: '/testtasktest/notifications/announcements',
       },
       read: {
         url: '/task/notifications/{id}',
