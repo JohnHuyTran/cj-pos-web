5f8d187ef8b5fe4d9cf6d597040f8503a9ae7c91
Supattra Buathong <a.supattra.nam@gmail.com>
2022-05-19 12:37:08 +0700

    fix bug

--- src/components/stock/stock-movement/stock-movement-search-list.tsx
+++ src/components/stock/stock-movement/stock-movement-search-list.tsx
@@ -104,7 +104,7 @@ function StockMovementSearchList() {
       headerAlign: 'center',
       sortable: false,
       renderCell: (params) => (
-        <Box component='div' sx={{ paddingLeft: '20px' }}>
+        <Box component="div" sx={{ paddingLeft: '20px' }}>
           {params.value}
         </Box>
       ),
@@ -113,7 +113,7 @@ function StockMovementSearchList() {
       field: 'createDate',
       headerClassName: 'columnHeaderTitle',
       headerName: 'วันที่ทำรายการ',
-      minWidth: 150,
+      minWidth: 132,
       flex: 0.35,
       headerAlign: 'center',
       sortable: false,
@@ -123,10 +123,9 @@ function StockMovementSearchList() {
           <div
             style={{
               textAlign: 'center',
-            }}>
-            <Typography variant='body2' noWrap>
-              {`${moment(date).add(543, 'year').format('DD/MM/YYYY')} ${moment(date).format('HH:mm ')}`}
-            </Typography>
+            }}
+          >
+            {`${moment(date).add(543, 'year').format('DD/MM/YYYY')} ${moment(date).format('HH:mm')}`}
           </div>
         );
       },
@@ -159,10 +158,11 @@ function StockMovementSearchList() {
         if (params.getValue(params.id, 'movementAction') === true && docNo) {
           return (
             <Typography
-              color='secondary'
-              variant='body2'
+              color="secondary"
+              variant="body2"
               sx={{ textDecoration: 'underline' }}
-              onClick={() => showDocumentDetail(docNo, docRef, docType, movementTypeCode)}>
+              onClick={() => showDocumentDetail(docNo, docRef, docType, movementTypeCode)}
+            >
               {params.value}
             </Typography>
           );
@@ -175,7 +175,7 @@ function StockMovementSearchList() {
       field: 'docRefNo',
       headerClassName: 'columnHeaderTitle',
       headerName: 'เลขที่เอกสารอ้างอิง',
-      minWidth: 100,
+      minWidth: 130,
       flex: 0.35,
       headerAlign: 'center',
       sortable: false,
@@ -413,7 +413,7 @@ function StockMovementSearchList() {
     <React.Fragment>
       <Box
         mt={2}
-        bgcolor='background.paper'
+        bgcolor="background.paper"
         sx={{
           '& .columnHeaderTitle-BG': {
             backgroundColor: '#20AE79',
@@ -425,7 +425,8 @@ function StockMovementSearchList() {
           '& .columnFilled-BG': {
             backgroundColor: '#E7FFE9',
           },
-        }}>
+        }}
+      >
         <div className={classes.MdataGridPaginationTopStock} style={{ height: rows.length >= 10 ? '80vh' : 'auto' }}>
           <DataGrid
             rows={rows}
@@ -438,7 +439,7 @@ function StockMovementSearchList() {
             pageSize={pageSize}
             rowsPerPageOptions={[10, 20, 50, 100]}
             rowCount={items.total}
-            paginationMode='server'
+            paginationMode="server"
             onPageChange={handlePageChange}
             onPageSizeChange={handlePageSizeChange}
             onCellClick={currentlySelected}
