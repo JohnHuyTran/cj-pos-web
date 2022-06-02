1235b178807ee44b4ff1c941bf7488583d689cbc
vtanh4 <vtanh4@cmcglobal.vn>
2022-06-02 11:33:21 +0700

    fix error run unit test

--- src/tests/components/common/ui/search-branch.test.tsx
+++ src/tests/components/common/ui/search-branch.test.tsx
@@ -19,6 +19,22 @@ beforeEach(() => {
   store = mockStore(initialState);
 });
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: jest.fn(),
+  },
+}));
+
 describe('component search branch', () => {
   it('check the box', () => {
     const handleOnclick = jest.fn();
