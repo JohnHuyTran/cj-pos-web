8fadec044d97c4e48bb8ed0e35bf1eaeb8b3e914
Supattra Buathong <a.supattra.nam@gmail.com>
2022-06-07 14:53:11 +0700

    improve dropdown status

--- src/utils/enum/purchase-branch-enum.ts
+++ src/utils/enum/purchase-branch-enum.ts
@@ -1,9 +1,10 @@
 export const purchaseBranchStatus = [
   { key: 'DRAFT', value: 'DRAFT', stepperGrp: 1 },
-  { key: 'SUBMITTED', value: 'SUBMITTED', stepperGrp: 2 },
-  { key: 'CREATED_PO', value: 'CREATED_PO', stepperGrp: 2 },
   { key: 'WAITING_BRANCH_EXAMINE', value: 'WAITING_BRANCH_EXAMINE', stepperGrp: 2 },
-  { key: 'CLOSED', value: 'CLOSED', stepperGrp: 3 },
+  { key: 'WAITING_DC_CHECK_DIFF', value: 'WAITING_DC_CHECK_DIFF', stepperGrp: 2 },
+  { key: 'DC_NO_STOCK', value: 'DC_NO_STOCK', stepperGrp: 2 },
+  { key: 'RECEIVE_COMPLETE', value: 'RECEIVE_COMPLETE', stepperGrp: 2 },
+  { key: 'INCOMPLETE_RECEIVED', value: 'INCOMPLETE_RECEIVED', stepperGrp: 2 },
 ];
 export const getPurchaseBranchStatusInfo = (key: string) => purchaseBranchStatus.find((item) => item.key === key);
 
