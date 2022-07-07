a3d0f309f1f35f081d280b067684a712c755a997
Supattra Buathong <a.supattra.nam@gmail.com>
2022-07-07 11:30:06 +0700

    common ui date picker

--- src/components/commons/ui/date-picker-css.tsx
+++ src/components/commons/ui/date-picker-css.tsx
@@ -35,7 +35,7 @@ const useStyles = makeStyles({
     },
     '& .MuiInputBase-root.MuiOutlinedInput-root': {
       height: '38px !important',
-    }
+    },
   },
   MdatepickerDetail: {
     '& .MuiOutlinedInput-input': {
@@ -79,14 +79,54 @@ const useStyles = makeStyles({
     },
     '& .MuiInputBase-root.MuiOutlinedInput-root': {
       height: '38px !important',
-    }
+    },
   },
+  MdatepickerMulti: {
+    '& .rmdp-container': {
+      width: '100%',
+      maxWidth: '100%',
 
-  // MkeyboardDatePicker: {
-  //   "& .MuiPickersDay-dayS": {
-  //     backgroundColor: "#36C690",
-  //   },
-  // },
+      '& > .rmdp-input': {
+        fontFamily: 'Kanit',
+        fontSize: '16px',
+        width: '100%',
+        border: '1px solid #CBD4DB',
+        borderRadius: '5px',
+        height: '40px',
+        paddingLeft: '38px',
+        cursor: 'pointer',
+        background:
+          'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFsAAABlCAMAAAAVmG92AAAAOVBMVEUAAADK1drK0tnHz9/K1NrPz8/L1NzK1NrM1tnL09vI09vL1NvK09rL1dvL09zL1dvKz9rPz9/L1NsZA7LtAAAAEnRSTlMA36AgYBDvkFBAcMWQf6+vMBCNayWVAAAA5ElEQVRo3u3ZbQuCMBSG4Z29lW+l5///2GZsmRgocU5ZPvfHMS6G4Aab+XwnKp0F5s2zXPJb58GGfVA7dFeailyKtGhtXnP17lm2LFv3oHti6Sgv3RHLR+FuE2vUjLRnnZSWXeyKcwp2zTkFu2WlZv9t7SS6vLS9kcjChn04O5JEcWELBhv24Wycl7Bh79le3Oesj22z3xuDDRt2KrhS2Dq2i/0ENux/tnFewoa9PxvnJWzY37VbxfvYmmf9zJvXEFknk+o4p2APxBpRopW+eHkw9lGe7pOr8xrdODNVWTme2mokbx5UJLPg2tGKAAAAAElFTkSuQmCC) 8px 50% no-repeat',
+
+        backgroundSize: 'auto 18px',
+        margin: '0px 0px',
+        padding: '18px 5px',
+      },
+    },
+    '& .rmdp-arrow': {
+      border: 'solid #686b6d',
+      borderWidth: '0 2px 2px 0',
+    },
+    '& .rmdp-arrow-container:hover': {
+      backgroundColor: '#d6d8da',
+    },
+    '& .rmdp-week-day': {
+      color: '#36C690',
+    },
+    '& .rmdp-day.rmdp-today span': {
+      background: '#fff',
+      border: '1px solid #36C690',
+      color: '#36C690',
+    },
+    '& .rmdp-day.rmdp-selected span': {
+      background: '#36C690',
+      color: '#fff',
+    },
+    '& .rmdp-day:not(.rmdp-disabled):not(.rmdp-day-hidden) span:hover': {
+      color: '#36C690',
+      backgroundColor: '#E7FFE9',
+    },
+  },
 });
 
 export { useStyles };
