ee8ba615456695a3f93751f60912e38bc25eb568
thiranan.jit <thiranan.jit@cjexpress.co.th>
2022-07-13 21:56:27 +0700

    clearDataSearchBranchAccounting

--- src/store/slices/accounting/accounting-search-slice.ts
+++ src/store/slices/accounting/accounting-search-slice.ts
@@ -3,7 +3,7 @@ import { ExpenseSearchRequest, ExpenseSearchResponse } from '../../../models/bra
 import { environment } from '../../../environment-base';
 import { get } from '../../../adapters/posback-adapter';
 
-// import { BranchAccountingSearch } from '../../../mockdata/branch-accounting-search';
+import { BranchAccountingSearch } from '../../../mockdata/branch-accounting-search';
 
 type State = {
   branchAccountingList: ExpenseSearchResponse;
@@ -27,46 +27,53 @@ const initialState: State = {
   error: '',
 };
 
-export const featchBranchAccountingListAsync = createAsyncThunk('ExpenseSearchRequest',
-async (payload: ExpenseSearchRequest) => {
-  try {
-    const apiRootPath = environment.branchAccounting.expense.search.url;
-    let path = `${apiRootPath}?limit=${payload.limit}&page=${payload.page}&type=${payload.type}`+
-    `&month=${payload.month}&year=${payload.year}`;
-    
-    if (payload.branchCode !== "") {
-      path = `${path}&branchCode=${payload.branchCode}`;
+export const featchBranchAccountingListAsync = createAsyncThunk(
+  'ExpenseSearchRequest',
+  async (payload: ExpenseSearchRequest) => {
+    try {
+      const apiRootPath = environment.branchAccounting.expense.search.url;
+      let path =
+        `${apiRootPath}?limit=${payload.limit}&page=${payload.page}&type=${payload.type}` +
+        `&month=${payload.month}&year=${payload.year}`;
+
+      if (payload.branchCode !== '') {
+        path = `${path}&branchCode=${payload.branchCode}`;
+      }
+      if (payload.status !== 'ALL') {
+        path = `${path}&status=${payload.status}`;
+      }
+      if (payload.period !== 0) {
+        path = `${path}&period=${payload.period}`;
+      }
+      let response: ExpenseSearchResponse = {
+        ref: '',
+        timestamp: '',
+        prev: 0,
+        next: 0,
+        code: 0,
+        message: '',
+        data: [],
+        total: 0,
+        page: 0,
+        perPage: 0,
+        totalPage: 0,
+      };
+      response = await get(path).then();
+      return response;
+
+      // return BranchAccountingSearch;
+    } catch (error) {
+      throw error;
     }
-    if (payload.status !== "ALL") {
-      path = `${path}&status=${payload.status}`;
-    }
-    if (payload.period !== 0) {
-      path = `${path}&period=${payload.period}`;
-    }
-    let response: ExpenseSearchResponse = {
-      ref: '',
-      timestamp: '',
-      prev: 0,
-      next: 0,
-      code: 0,
-      message: '',
-      data: [],
-      total: 0,
-      page: 0,
-      perPage: 0,
-      totalPage: 0,
-    }
-    response = await get(path).then();
-    return response;
-  } catch (error) {
-    throw error;
   }
-});
+);
 
 const searchBranchAccountingSlice = createSlice({
   name: 'searchBranchAccounting',
   initialState,
-  reducers: {},
+  reducers: {
+    clearDataSearchBranchAccounting: (state) => initialState,
+  },
   extraReducers: (builer) => {
     builer.addCase(featchBranchAccountingListAsync.pending, () => {
       initialState;
@@ -80,5 +87,5 @@ const searchBranchAccountingSlice = createSlice({
   },
 });
 
-// export const { clearDataFilter } = dcCheckOrderSlice.actions;
+export const { clearDataSearchBranchAccounting } = searchBranchAccountingSlice.actions;
 export default searchBranchAccountingSlice.reducer;
