1159fa9ade56b0b1081750bd0ab8f6df6ef4453f
Kitsana <kitsana.sap@cjexpress.co.th>
2022-07-14 09:30:00 +0700

    fix: button loading to modal loading.

--- src/components/accounting/expense/search-expense.tsx
+++ src/components/accounting/expense/search-expense.tsx
@@ -6,16 +6,18 @@ import {
   Select,
   MenuItem,
   Button,
-  CircularProgress,
+  // CircularProgress,
   FormHelperText,
 } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
+// import { LoadingButton } from '@mui/lab';
 import { useStyles } from '../../../styles/makeTheme';
 import { Upload, AddCircleOutline } from '@mui/icons-material';
 import { expenseTypes, expenseStatusList } from '../../../utils/enum/accounting-enum';
 import { PERMISSION_GROUP } from '../../../utils/enum/permission-enum';
 import DatePickerMonth from '../../../components/commons/ui/date-picker-month';
 import { useAppSelector, useAppDispatch } from '../../../store/store';
+import ModalSelectPeriod from '../expense/modal-select-period';
+import LoadingModal from '../../commons/ui/loading-modal';
 
 // Import File ที่เกี่ยวข้องกับ Business Logic Select สาขา
 import BranchListDropDown from '../../../components/commons/ui/branch-list-dropdown';
@@ -32,7 +34,6 @@ import {
   clearDataExpensePeriod,
   featchExpensePeriodTypeAsync,
 } from '../../../store/slices/accounting/accounting-period-type-slice';
-import ModalSelectPeriod from '../expense/modal-select-period';
 
 interface FormSelectProps {
   title: string;
@@ -329,24 +330,26 @@ export default function SearchExpense() {
           >
             เคลียร์
           </Button>
-          <LoadingButton
+          <Button
             id="btnSearch"
             variant="contained"
             color="primary"
+            disabled={isOpenLoading}
             onClick={handleSearchExpense}
-            loading={isOpenLoading}
-            loadingIndicator={
-              <Typography component="span" sx={{ fontSize: '11px' }}>
-                กรุณารอสักครู่ <CircularProgress color="inherit" size={15} />
-              </Typography>
-            }
+            // loading={isOpenLoading}
+            // loadingIndicator={
+            //   <Typography component="span" sx={{ fontSize: '11px' }}>
+            //     กรุณารอสักครู่ <CircularProgress color="inherit" size={15} />
+            //   </Typography>
+            // }
             sx={{ width: '170.42px', ml: 2 }}
             className={classes.MbtnSearch}
           >
             ค้นหา
-          </LoadingButton>
+          </Button>
         </Grid>
       </Grid>
+      <LoadingModal open={isOpenLoading} />
 
       {openSelectPeriod && (
         <ModalSelectPeriod open={openSelectPeriod} onClose={handleCloseSelectPeriodModal} type={types} />
