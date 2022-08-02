c7687498925eadce216ac67d0614e08c3b9bce85
Nungruthai Pangnuja <nungruthai.pang@cjexpress.co.th>
2022-08-02 17:31:31 +0700

    update default value

--- src/components/accounting/open-end/modal-detail-cash.tsx
+++ src/components/accounting/open-end/modal-detail-cash.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent } from '@mui/material';
+import { Dialog, DialogContent, Grid } from '@mui/material';
 import { BootstrapDialogTitle } from 'components/commons/ui/dialog-title';
 import React from 'react';
 
@@ -12,7 +12,37 @@ function ModalDetailCash({ isOpen, onClose }: Props) {
     <React.Fragment>
       <Dialog open={isOpen} maxWidth='xl' fullWidth={true}>
         <BootstrapDialogTitle id='customized-dialog-title' onClose={onClose}></BootstrapDialogTitle>
-        <DialogContent sx={{ minHeight: '70vh' }}></DialogContent>
+        <DialogContent sx={{ minHeight: '70vh' }}>
+          <Grid container spacing={1} padding={2} minWidth={'1000px'}>
+            <Grid
+              item
+              xs={7}
+              pr={1}
+              mt={1}
+              sx={{
+                // backgroundColor: '#f3fbf8',
+                border: '1px solid #EAEBEB',
+                borderRadius: '7px',
+                padding: '20px 40px 20px 10px',
+              }}>
+              รายการรับตามประเภทชำระ
+            </Grid>
+
+            <Grid
+              item
+              xs={5}
+              pr={1}
+              mt={1}
+              sx={{
+                // backgroundColor: '#f3fbf8',
+                border: '1px solid #EAEBEB',
+                borderRadius: '7px',
+                padding: '20px 40px 20px 10px',
+              }}>
+              รายการรับตามประเภท
+            </Grid>
+          </Grid>
+        </DialogContent>
       </Dialog>
     </React.Fragment>
   );
