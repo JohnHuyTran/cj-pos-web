2c32da3a0c6a4a593bcb05d33f1d8be321b9cf6b
Nungruthai Pangnuja <nungruthai.pang@cjexpress.co.th>
2022-08-03 15:35:29 +0700

    update status

---
+++ src/store/slices/accounting/open-end-slice.ts
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { get } from 'adapters/posback-adapter';
+import { environment } from 'environment-base';
+import { ViewOpenEndResponse } from 'models/branch-accounting-model';
+import { getPathExpenseDetail } from 'services/accounting';
+
+type State = {
+  viewOpenEnd: ViewOpenEndResponse;
+  error: string;
+};
+
+const initialState: State = {
+  viewOpenEnd: {
+    timestamp: '',
+    ref: '',
+    code: 0,
+    message: '',
+    data: null,
+  },
+  error: '',
+};
+
+export const featchOpenEndDeatilAsync = createAsyncThunk('OpenEndDeatil', async (docNo: string) => {
+  try {
+    const apiRootPath = getPathExpenseDetail(docNo, environment.branchAccounting.expense.detail.url);
+    return await get(apiRootPath).then();
+  } catch (error) {
+    throw error;
+  }
+});
+
+const viewOpenEndSlice = createSlice({
+  name: 'OpenEndDeatil',
+  initialState,
+  reducers: {
+    clearDataFilter: (state) => initialState,
+  },
+  extraReducers: (builer) => {
+    builer.addCase(featchOpenEndDeatilAsync.pending, () => {
+      initialState;
+    }),
+      builer.addCase(featchOpenEndDeatilAsync.fulfilled, (state, action: PayloadAction<any>) => {
+        state.viewOpenEnd = action.payload;
+      }),
+      builer.addCase(featchOpenEndDeatilAsync.rejected, () => {
+        initialState;
+      });
+  },
+});
+
+export const { clearDataFilter } = viewOpenEndSlice.actions;
+export default viewOpenEndSlice.reducer;
