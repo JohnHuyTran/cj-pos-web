3a57c71ede7c32ec8b435de79fb1c21cd75c2337
Nungruthai Pangnuja <nungruthai.pang@cjexpress.co.th>
2022-08-03 22:41:46 +0700

    update layout

--- src/components/accounting/open-end/modal-detail-cash.tsx
+++ src/components/accounting/open-end/modal-detail-cash.tsx
@@ -25,7 +25,7 @@ function ModalDetailCash({ isOpen, onClose }: Props) {
       <Dialog open={isOpen} maxWidth='lg' fullWidth={true}>
         <BootstrapDialogTitle id='customized-dialog-title' onClose={onClose}></BootstrapDialogTitle>
         <DialogContent>
-          <Grid container spacing={2.5} padding={2} minWidth={'1000px'}>
+          <Grid container spacing={3} padding={2} minWidth={'1000px'}>
             <Grid item mt={1} xs={6}>
               <Grid
                 container
@@ -72,7 +72,116 @@ function ModalDetailCash({ isOpen, onClose }: Props) {
             </Grid>
 
             <Grid item xs={6} mt={1}>
-              <Grid
+              <Grid container height='100%' direction='column' justifyContent='space-between'>
+                <Grid item>
+                  <Grid
+                    sx={{
+                      // backgroundColor: '#f3fbf8',
+                      border: '1px solid #EAEBEB',
+                      borderRadius: '7px',
+                      padding: '10px 40px 20px 10px',
+                    }}
+                    container
+                    spacing={2}
+                    mb={3}>
+                    {incomes && incomes.typeItems.length > 0 && (
+                      <Grid item xs={12}>
+                        <Typography sx={{ fontWeight: 'bold' }}>รายรับตามประเภท</Typography>
+                      </Grid>
+                    )}
+                    {incomes &&
+                      incomes.typeItems.length > 0 &&
+                      incomes.typeItems.map((element: Item, index: number) => {
+                        return (
+                          <>
+                            <Grid item xs={1}>
+                              <Typography></Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                              <Typography>{element.name}</Typography>
+                            </Grid>
+                            <Grid item xs={5}>
+                              <NumberFormat
+                                value={String(element.amount)}
+                                thousandSeparator={true}
+                                decimalScale={2}
+                                className={classes.MtextFieldNumber}
+                                disabled={true}
+                                customInput={TextField}
+                                fixedDecimalScale
+                              />
+                            </Grid>
+                          </>
+                        );
+                      })}
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Grid
+                    sx={{
+                      // backgroundColor: '#f3fbf8',
+                      // border: '1px solid #BFF1C4',
+                      // borderRadius: '7px',
+                      padding: '0px 40px 20px 10px',
+                    }}
+                    container
+                    spacing={2}
+                    mb={3}>
+                    <Grid item xs={1}>
+                      <Typography></Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography>รวมยอดขาย(ยกเว้นภาษี)</Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                      <NumberFormat
+                        value={String(incomes && incomes.totalAmount ? incomes.totalAmount : 0)}
+                        thousandSeparator={true}
+                        decimalScale={2}
+                        className={classes.MtextFieldNumber}
+                        disabled={true}
+                        customInput={TextField}
+                        fixedDecimalScale
+                      />
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography></Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography>ยอดขายก่อนภาษี</Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                      <NumberFormat
+                        value={String(incomes && incomes.netAmount ? incomes.netAmount : 0)}
+                        thousandSeparator={true}
+                        decimalScale={2}
+                        className={classes.MtextFieldNumber}
+                        disabled={true}
+                        customInput={TextField}
+                        fixedDecimalScale
+                      />
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography></Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography>ยอดขายยกเว้นภาษี</Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                      <NumberFormat
+                        value={String(incomes && incomes.netAmountNonVat ? incomes.netAmountNonVat : 0)}
+                        thousandSeparator={true}
+                        decimalScale={2}
+                        className={classes.MtextFieldNumber}
+                        disabled={true}
+                        customInput={TextField}
+                        fixedDecimalScale
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              {/* <Grid
                 sx={{
                   // backgroundColor: '#f3fbf8',
                   border: '1px solid #EAEBEB',
@@ -112,8 +221,8 @@ function ModalDetailCash({ isOpen, onClose }: Props) {
                       </>
                     );
                   })}
-              </Grid>
-              <Grid
+              </Grid> */}
+              {/* <Grid
                 sx={{
                   // backgroundColor: '#f3fbf8',
                   // border: '1px solid #BFF1C4',
@@ -173,7 +282,7 @@ function ModalDetailCash({ isOpen, onClose }: Props) {
                     fixedDecimalScale
                   />
                 </Grid>
-              </Grid>
+              </Grid> */}
             </Grid>
           </Grid>
         </DialogContent>
